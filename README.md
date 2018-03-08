b5c8230a5fd40df8997312f7fae5950e0704a51d
Sunil Patel <sunil2197@gmail.com>
2018-03-08 18:54:01 +0530

    passport OAuth

---
+++ README.md
@@ -0,0 +1,12 @@
+# Node Authentication
+
+##### Uses Passport to authenticate and connect accounts storing account data in a MongoDB
+
+##### Accounts include:
+* Local Account
+* Google
+* Twitter
+* Github
+* Instagram
+* Facebook
+* LinkedIn
---
+++ config/auth.js
@@ -0,0 +1,28 @@
+module.exports = {
+	'twitterAuth' : {
+		'consumerKey': 'hk2SQTiWIecV8eHBRrcVe0AQd',
+		'consumerSecret': 'lsAklTMPqD3XczfDut0LQis9EECbcHjxQWhcYqKB2vHGJu3hor',
+		'callbackURL': 'http://127.0.0.1:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': '816595753414-c927u5sdcfki5otngndsan7t87dhich5.apps.googleusercontent.com',
+		'clientSecret': 'IUWkkPok-JyrgiIXsODPj3Vs',
+		'callbackURL': 'http://127.0.0.1:8080/auth/google/callback'
+	},
+	'githubAuth': {
+    	'clientID': 'f3c7995821029e2abb43',
+    	'clientSecret': '9e4e11708ac103b9af88054ad230ffd4a8588cea',
+    	'callbackURL': 'http://127.0.0.1:8080/auth/github/callback'
+  	},
+  	'linkedinAuth': {
+    	'consumerKey': '81dw0ag2zyvf9e',
+    	'consumerSecret': 'EWTh6P61pqsuQzD9',
+    	'callbackURL': 'http://127.0.0.1:8080/auth/linkedin/callback'
+  	},
+  	'instagramAuth': {
+    	'clientID': '179fb94cc06d488f983aa3b05b63cf90',
+    	'clientSecret': '6cead60cd93f4b52bdac12e51d561c84',
+    	'callbackURL': 'http://127.0.0.1:8080/auth/instagram/callback'
+  	}
+};
